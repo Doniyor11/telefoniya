558a3e8b2d6a4b5902494aaec203c39f73527a26
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-07-02 18:54:02 +0200

    main.js populated with CangeColorTheme class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// to be extended... currently only swaps color ←→ background-color
+class ChangeColorTheme {
+    constructor(target) {
+        // use string for the target argument, eg ".switch-color"
+        this.target = document
+            .querySelector(target)
+            .addEventListener("click", this.toggleTheme.bind(this))
+
+        this.fg = window
+            .getComputedStyle(document.body)
+            .getPropertyValue("background-color")
+
+        this.bg = window
+            .getComputedStyle(document.body)
+            .getPropertyValue("color")
+    }
+
+    toggleTheme() {
+        [this.fg, this.bg] = [this.bg, this.fg];
+        document.body.style.backgroundColor = this.fg;
+        document.body.style.color = this.bg;
+    }
+}
